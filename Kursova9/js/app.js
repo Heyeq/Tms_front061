16d4e7d770031892666e996ea087671577a01973
ANDREI SIVIRYNAU (Heyeq) <sasivirinov32@yandex.by>
2024-05-17 04:24:50 +0300

    created section TABS

--- Kursova9/js/app.js
+++ Kursova9/js/app.js
@@ -1,1 +1,5 @@
-console.log ("Hola")
+$(document).ready(function(){
+    $('.burger__map').click(function(event){
+        $('.burger__map, .navigation-list').toggleClass('active');
+    });
+    });
